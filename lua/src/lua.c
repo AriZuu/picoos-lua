54de628404872a588d98acd34222dd8344ea99ba
Ari Suutari <ari@stonepile.fi>
2015-06-01 21:04:24 +0300

    Revert linit & lua main to stock versions, as they are no longer actually used by this library.

--- lua/src/lua.c
+++ lua/src/lua.c
@@ -592,7 +592,7 @@ static int pmain (lua_State *L) {
 }
 
 
-int luaMain (int argc, char **argv) {
+int main (int argc, char **argv) {
   int status, result;
   lua_State *L = luaL_newstate();  /* create state */
   if (L == NULL) {
