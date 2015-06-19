9fe26c882c93e640e1e80d5f6b482daaab0bab87
Ari Suutari <ari@stonepile.fi>
2015-06-19 15:37:31 +0300

    Fix problem with LUAL_BUFFERSIZE in 5.3.1.

--- lua/src/luaconf.h
+++ lua/src/luaconf.h
@@ -717,7 +717,7 @@
 ** smaller buffer would force a memory allocation for each call to
 ** 'string.format'.)
 */
-#if defined(LUA_FLOAT_LONGDOUBLE)
+#if LUA_FLOAT_TYPE == LUA_FLOAT_LONGDOUBLE
 #define LUAL_BUFFERSIZE		8192
 #else
 #define LUAL_BUFFERSIZE   ((int)(0x80 * sizeof(void*) * sizeof(lua_Integer)))
